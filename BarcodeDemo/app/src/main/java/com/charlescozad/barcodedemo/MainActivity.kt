41ab660c506f20018a1a1f66c51dd91141a12bed
ccozad <ccozad@gmail.com>
2025-02-05 17:23:44 -0800

    Trim more fat, update notes

--- BarcodeDemo/app/src/main/java/com/charlescozad/barcodedemo/MainActivity.kt
+++ BarcodeDemo/app/src/main/java/com/charlescozad/barcodedemo/MainActivity.kt
@@ -8,35 +8,14 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.charlescozad.barcodedemo.ui.theme.BarcodeDemoTheme
 import java.util.ArrayList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
-        /*enableEdgeToEdge()
-        setContent {
-            BarcodeDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }*/
         val button: Button = findViewById(R.id.barcodeButton)
         button.setOnClickListener {
             val barcodeScanner = Intent(this, LivePreviewActivity::class.java)
@@ -88,32 +67,12 @@ class MainActivity : ComponentActivity() {
     }
 
     companion object {
-        private const val TAG = "EntryChoiceActivity"
+        private const val TAG = "MainActivity"
         private const val PERMISSION_REQUESTS = 1
 
         private val REQUIRED_RUNTIME_PERMISSIONS =
             arrayOf(
-                Manifest.permission.CAMERA,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE
+                Manifest.permission.CAMERA
             )
-    }
-}
-
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BarcodeDemoTheme {
-        Greeting("Android")
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# ml-kit-reference-app
+# Introduction
+
+This repo has reference implementations for using ML Kit for different tasks.
+
+# Bar code demo
+
+Process bar codes with the camera.
+
+## Technical notes
+
+### Permisions
+
+Required permissions:
+ - Camera
+
+ ```kotlin
+private fun isPermissionGranted(context: Context, permission: String): Boolean {
+    if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+    ) {
+        Log.i(TAG, "Permission granted: $permission")
+        return true
+    }
+    Log.i(TAG, "Permission NOT granted: $permission")
+    return false
+}
+ ```
+
+
+### LivePreviewActivity
+
+This activity has a selection dropdown that brings in different models that act on camera previw data. There is also basic functionality to switch from rear to front facing cameras. The screen links to a settings screen.
+
+This was part of a larger demo in the ML Kit vision section that showed many different types of models. Most of that functionality has been removed to focus in on the barcode processing alone.
+
+Process:
+
+1. A camera source is setup
+2. A machine learning frame processor processes data coming in from the camera
+3. Data processing results are sent to the live preview rendering surface
+
+### BarcodeScannerProcessor
+
+### VisionImageProcessor
+
+### VisionProcessorBase
+
+
